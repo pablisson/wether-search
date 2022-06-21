3bb04712b84dc3ac56022bfa19efd99fd12be756
Pablisson <pablissonaraujo@gmail.com>
2022-06-21 01:35:04 -0300

    Atualização na visualização dos itens

--- web-service/weatherApp/views.py
+++ web-service/weatherApp/views.py
@@ -108,8 +108,14 @@ class json_manager:
 
   def get_item_from_json(self, i):
     main_item = "".join([self.item_name,"[",str(i),"]"])
-    print(main_item)
     return jsonpath.jsonpath(self.json_response, main_item)
+  
+  def size_of_json(self):
+    allItems = self.get_item_from_json_all()
+    print(len(allItems[0]))
+    if allItems is not None:
+      return len(allItems[0])
+    return 0
 
 
 class items_for_viewer:
@@ -123,7 +129,34 @@ class items_for_viewer:
   def create_jason_list(self, region):
     manager_data = json_manager()
     manager_data.get_weather_by_region(region)  
-    itemAux = manager_data.get_item_from_json(0)
+    itemAux = manager_data.get_item_from_json(6)
+    allItems = manager_data.get_item_from_json_all()
+
+    j = 0
+    dataAux = ""
+    countItems = manager_data.size_of_json()
+    print(countItems)
+
+    #print(item)
+    """
+    for i in range(0, 5): 
+      itemAux = manager_data.get_item_from_json(i)
+      print(itemAux[0][self.labels[0]])
+      print(itemAux[0]['main'][self.labels[1]])
+      print(itemAux[0]['main'][self.labels[2]])
+      print(itemAux[0]['main'][self.labels[3]])
+      print(itemAux[0]['main'][self.labels[4]])
+      print(itemAux[0]['main'][self.labels[5]])
+      print(itemAux[0]['main'][self.labels[6]])
+
+      print(itemAux[0]['weather'][0][self.labels[7]])
+      
+      print(itemAux[0]['weather'][0][self.labels[8]])
+      print(itemAux[0]['wind'][self.labels[9]])
+      print(itemAux[0]['wind'][self.labels[10]])
+      print(itemAux[0]['clouds'][self.labels[11]])
+      #print(itemAux)
+"""
     """
     #self.add_item(self.labels[0], itemAux[0][self.labels[0]])
     self.items[self.labels[0]] = itemAux[0][self.labels[0]]
@@ -142,12 +175,11 @@ class items_for_viewer:
     self.items[self.labels[11]] = itemAux[0]['clouds'][self.labels[11]]
 """
 
-    print(self.items)
-    for i in range(0,5):
-      print(i)
+    #print(self.items)
+
       
-    
-    for i in range(0, 5): 
+    """
+    for i in range(0, 2): 
       itemAux = manager_data.get_item_from_json(i)
       print(itemAux)
       self.items[self.labels[0]] = itemAux[i][self.labels[0]]
@@ -157,12 +189,12 @@ class items_for_viewer:
       self.items[self.labels[4]] = itemAux[i]['main'][self.labels[4]]
       self.items[self.labels[5]] = itemAux[i]['main'][self.labels[5]]
       self.items[self.labels[6]] = itemAux[i]['main'][self.labels[6]]
-      self.items[self.labels[7]] = itemAux[i]['weather'][self.labels[7]]
-      self.items[self.labels[8]] = itemAux[i]['weather'][self.labels[8]]
+      self.items[self.labels[7]] = itemAux[i]['weather'][0][self.labels[7]]
+      self.items[self.labels[8]] = itemAux[i]['weather'][0][self.labels[8]]
       self.items[self.labels[9]] = itemAux[i]['wind'][self.labels[9]]
       self.items[self.labels[10]] = itemAux[i]['wind'][self.labels[10]]
       self.items[self.labels[11]] = itemAux[i]['clouds'][self.labels[11]]
-    
+  """  
   
     return self.items
     
