85c0ca5bd5bb34d6e5406687734b0674ac5dfae4
Pablisson <pablissonaraujo@gmail.com>
2022-06-24 00:03:20 -0300

    remoção de arquivos não utilizados

--- weatherApp/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from weatherApp.models import ClimaTempo
-
-class ClimaTempoSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = ClimaTempo
-    fields = ['data', 'temperatura', 'pressao', 'humidade', 'vento', 'umidade', 'descricao', 'icon', 'cidade', 'estado', 'pais', 'latitude', 'longitude']
--- weatherApp/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from weatherApp.models import ClimaTempo
-from weatherApp.serializer import ClimaTempoSerializer
-
-class ClimaTempoViewSet(viewsets.ModelViewSet):
-  queryset = ClimaTempo.objects.all()
-  serializer_class = ClimaTempoSerializer
